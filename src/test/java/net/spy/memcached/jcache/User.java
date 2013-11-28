abacd69c6739e074b4a72ac89d7ebed3cc28e182
Jacky Chan <libing.chen@gmail.com>
2013-11-28 19:43:24 +0800

    user object

---
+++ src/test/java/net/spy/memcached/jcache/User.java
@@ -0,0 +1,29 @@
+package net.spy.memcached.jcache;
+
+import java.io.Serializable;
+
+/**
+ * user object
+ *
+ * @author linux_china
+ */
+public class User implements Serializable {
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
